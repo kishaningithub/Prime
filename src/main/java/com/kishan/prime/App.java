a22a92fc9a95942228735cdc70d76c1533bdfab3
Kishan B <b.kishan.n@gmail.com>
2016-01-31 13:40:52 +0000

    Code refactor

--- src/main/java/com/kishan/prime/App.java
+++ src/main/java/com/kishan/prime/App.java
@@ -5,28 +5,12 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ * Entry point class
  *
  */
 public class App 
 {
-//	  OkHttpClient client = new OkHttpClient();
-//
-//	  String run(String url) throws IOException {
-//	    Request request = new Request.Builder()
-//	        .url(url)
-//	        .build();
-//
-//	    Response response = client.newCall(request).execute();
-//	    return response.body().string();
-//	  }
-//
-//	  public static void main(String[] args) throws IOException {
-//	    App example = new App();
-//	    String response = example.run("https://primes.firebaseio.com/largestprime.json");
-//	    System.out.println(response);
-//	  }
-	
+
     public static void main( String[] args ) throws IOException
     {
 		System.out.println("How many more you want to generate? ");
